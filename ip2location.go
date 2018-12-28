510d965c21b1195489403905e6a1b9ba19695f57
catatsuy <catatsuy@catatsuy.org>
2018-12-28 13:06:43 +0900

    return err

--- ip2location.go
+++ ip2location.go
@@ -269,13 +269,13 @@ func readfloat(pos uint32) float32 {
 }
 
 // initialize the component with the database path
-func Open(dbpath string) {
+func Open(dbpath string) error {
 	max_ipv6_range.SetString("340282366920938463463374607431768211455", 10)
 
 	var err error
 	f, err = os.Open(dbpath)
 	if err != nil {
-		return
+		return err
 	}
 
 	meta.databasetype = readuint8(1)
@@ -373,11 +373,13 @@ func Open(dbpath string) {
 	}
 
 	metaok = true
+
+	return nil
 }
 
 // close database file handle
-func Close() {
-	f.Close()
+func Close() error {
+	return f.Close()
 }
 
 // get api version
@@ -411,7 +413,7 @@ func loadmessage(mesg string) IP2Locationrecord {
 }
 
 // get all fields
-func Get_all(ipaddress string) IP2Locationrecord {
+func GetAll(ipaddress string) IP2Locationrecord {
 	return query(ipaddress, all)
 }
 
